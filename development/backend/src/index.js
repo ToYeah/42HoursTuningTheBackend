1c24ac19eedd3b72e17242e8468da7a13caf86a9
Tanesan <keito.12.10@icloud.com>
2022-04-16 08:49:13 +0900

    malticore

--- development/backend/src/index.js
+++ development/backend/src/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express();
+const compression = require('compression');
+const cluster = require('cluster');
+const numCpus = require('os').cpus().length;
 
-app.use(express.json({limit: '10mb'}))
+app.use(compression(express.json({limit: '10mb'})))
 
 const api = require("./api");
 
@@ -128,5 +131,19 @@ app.get('/api/client/records/:recordId/files/:itemId/thumbnail', async (req, res
 })
 
 
-app.listen(8000, () => console.log('listening on port 8000...'))
+cluster.schedulingPolicy = cluster.SCHED_NONE; // windowsの場合
 
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+
+  for (let i = 0; i < numCpus; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} was killed by signal: ${signal}`);
+    cluster.fork();
+  });
+} else {
+  app.listen(8000, () => console.log('listening on port 8000..'));
+}
