a8df4a7d28e2fe17d9833a555c893d6a7a81d4f9
Taisei Toya <tytaisei0102@gmail.com>
2022-04-16 06:56:35 +0200

    wip

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -1,1 +1,9 @@
 alter table session add index (value);
+
+alter table record add index (record_id);
+
+alter table group_member add index (user_id);
+
+alter table user add index (user_id);
+
+alter table group_info add index (group_id);
