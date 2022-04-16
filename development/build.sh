3470239dc9831a7d872697933a2f08d2002968da
Tanesan <keito.12.10@icloud.com>
2022-04-16 23:21:24 +0900

    option消す

--- development/build.sh
+++ development/build.sh
@@ -7,6 +7,6 @@
 # web画面の変更を反映したい場合、コメントアウトを外す。
 # (cd ./frontend && npm run build)
 
-docker-compose down --rmi all -DWITH_INNODB_MEMCACHED=ON
-docker-compose build --no-cache -DWITH_INNODB_MEMCACHED=ON
+docker-compose down --rmi all
+docker-compose build --no-cache
 docker-compose up -d
