1d2029d7dd96bf8e66d6b40328d46e4c4cb3df14
Tanesan <keito.12.10@icloud.com>
2022-04-16 23:46:46 +0900

    ドッカーの修正

--- development/mysql/sql/init.sql
+++ development/mysql/sql/init.sql
@@ -1,2 +1,2 @@
-source /usr/share/mysql/innodb_memcached_config.sql
-INSTALL PLUGIN daemon_memcached soname "libmemcached.so";
+-- source /usr/share/mysql/innodb_memcached_config.sql
+-- INSTALL PLUGIN daemon_memcached soname "libmemcached.so";
